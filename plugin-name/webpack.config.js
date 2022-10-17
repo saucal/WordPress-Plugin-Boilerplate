ce112a3f61cb874db42f11ebc51b48a3a724c5e9
Dimitrios Pantazis <dpanta94@gmail.com>
2022-10-17 13:28:22 +0300

    Producing min and not css files

--- plugin-name/webpack.config.js
+++ plugin-name/webpack.config.js
@@ -1,9 +1,73 @@
 const defaultConfig = require('@wordpress/scripts/config/webpack.config');
+const postcssPlugins = require( '@wordpress/postcss-plugins-preset' );
 const TerserPlugin = require('terser-webpack-plugin');
 const CopyWebpackPlugin = require( 'copy-webpack-plugin' );
+const MiniCSSExtractPlugin = require( 'mini-css-extract-plugin' );
 const path = require('path');
 const { fromProjectRoot } = require('@wordpress/scripts/utils/file');
 const fs = require('fs');
+
+const {
+	hasCssnanoConfig,
+	hasPostCSSConfig,
+	hasBabelConfig,
+	hasArgInCLI,
+} = require('@wordpress/scripts/utils');
+
+const hasReactFastRefresh = hasArgInCLI( '--hot' ) && ! isProduction;
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const cssLoaders = [
+	{
+		loader: MiniCSSExtractPlugin.loader,
+	},
+	{
+		loader: require.resolve( 'css-loader' ),
+		options: {
+			sourceMap: ! isProduction,
+			modules: {
+				auto: true,
+			},
+		},
+	}
+];
+
+const cssMinLoaders = [
+	...cssLoaders,
+	{
+		loader: require.resolve( 'postcss-loader' ),
+		options: {
+			// Provide a fallback configuration if there's not
+			// one explicitly available in the project.
+			...( ! hasPostCSSConfig() && {
+				postcssOptions: {
+					ident: 'postcss',
+					sourceMap: ! isProduction,
+					plugins: isProduction
+						? [
+								...postcssPlugins,
+								require( 'cssnano' )( {
+									// Provide a fallback configuration if there's not
+									// one explicitly available in the project.
+									...( ! hasCssnanoConfig() && {
+										preset: [
+											'default',
+											{
+												discardComments: {
+													removeAll: true,
+												},
+											},
+										],
+									} ),
+								} ),
+						  ]
+						: postcssPlugins,
+				},
+			} ),
+		},
+	},
+];
 
 function getWebpackEntryPoints() {
 	let entryPoints = {};
@@ -46,10 +110,8 @@ function getWebpackEntryPoints() {
 				const itemName = item.name.replace( '.js', '' ).replace('.scss', '' );
 				entryPoints[entryPath + '/' + dir + '/' + itemName] = path.resolve( entryPaths[entryPath], dir, item.name );
 
-				// If the files are JS include a minified version as well.
-				if ( 'js' === entryPath ) {
-					entryPoints[entryPath + '/' + dir + '/' + itemName + '.min'] = path.resolve( entryPaths[entryPath], dir, item.name );
-				}
+				// Produce minified versions of the files.
+				entryPoints[entryPath + '/' + dir + '/' + itemName + '.min'] = path.resolve( entryPaths[entryPath], dir, item.name );
 
 				return item.name;
 			});
@@ -58,6 +120,9 @@ function getWebpackEntryPoints() {
 
 	return entryPoints;
 }
+
+let scssFilesToMinify = [];
+let scssFilesMinified = [];
 
 module.exports = {
 	...defaultConfig,
@@ -72,6 +137,18 @@ module.exports = {
 		minimizer: [
 			new TerserPlugin({
 				parallel: true,
+				terserOptions: {
+					output: {
+						comments: /translators:/i,
+					},
+					compress: {
+						passes: 2,
+					},
+					mangle: {
+						reserved: [ '__', '_n', '_nx', '_x' ],
+					},
+				},
+				extractComments: false,
 				include: [ /\.min\.js$/ ],
 			}),
 		],
@@ -79,12 +156,123 @@ module.exports = {
 	module: {
 		...defaultConfig.module,
 		rules: [
-			...defaultConfig.module.rules,
 			{
-				test: /\.(bmp|png|jpe?g|gif)$/i,
+				test: /\.(j|t)sx?$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: require.resolve( 'babel-loader' ),
+						options: {
+							// Babel uses a directory within local node_modules
+							// by default. Use the environment variable option
+							// to enable more persistent caching.
+							cacheDirectory:
+								process.env.BABEL_CACHE_DIRECTORY || true,
+
+							// Provide a fallback configuration if there's not
+							// one explicitly available in the project.
+							...( ! hasBabelConfig() && {
+								babelrc: false,
+								configFile: false,
+								presets: [
+									require.resolve(
+										'@wordpress/babel-preset-default'
+									),
+								],
+								plugins: [
+									hasReactFastRefresh &&
+										require.resolve(
+											'react-refresh/babel'
+										),
+								].filter( Boolean ),
+							} ),
+						},
+					},
+				],
+			},
+			{
+				test: /\.css$/,
+				use: cssLoaders,
+			},
+			{
+				test: /\.min\.css$/,
+				use: cssMinLoaders,
+			},
+			{
+				// Testing for scss files. Producing minified.
+				test: (name) => {
+					if ( 'scss' !== name.split('.').pop() ) {
+						return false;
+					}
+
+					if ( scssFilesToMinify.includes( name ) ) {
+						scssFilesMinified.push( name );
+						return true;
+					}
+
+					scssFilesToMinify.push( name );
+					return false;
+				},
+				use: [
+					...cssMinLoaders,
+					{
+						loader: require.resolve( 'sass-loader' ),
+						options: {
+							sourceMap: ! isProduction,
+						},
+					},
+				],
+			},
+			{
+				// Testing for scss files. Producing not minified.
+				test: (name) => {
+					if ( 'scss' !== name.split('.').pop() ) {
+						return false;
+					}
+
+					if ( scssFilesMinified.includes( name ) ) {
+						return false;
+					}
+
+					return true;
+				},
+				use: [
+					...cssLoaders,
+					{
+						loader: require.resolve( 'sass-loader' ),
+						options: {
+							sourceMap: ! isProduction,
+							sassOptions: {
+								minimize: false,
+								outputStyle: 'expanded'
+							}
+						},
+					},
+				],
+			},
+			{
+				test: /\.svg$/,
+				issuer: /\.(j|t)sx?$/,
+				use: [ '@svgr/webpack', 'url-loader' ],
+				type: 'javascript/auto',
+			},
+			{
+				test: /\.svg$/,
+				issuer: /\.(sc|sa|c)ss$/,
+				type: 'asset/inline',
+			},
+			{
+				test: /\.(bmp|png|jpe?g|gif|webp)$/i,
 				type: 'asset/resource',
 				generator: {
 					filename: 'images/[name][ext]',
+				},
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: 'asset/resource',
+				generator: {
+					filename: 'fonts/[name].[hash:8][ext]',
 				},
 			},
 		],
