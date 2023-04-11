fe3eec5f5d6c5b237e68eccfd01d8021d1838d27
Dimitrios Pantazis <dpanta94@gmail.com>
2023-04-11 21:53:00 +0300

    added rest_api_request to frontend request check

--- plugin-name/includes/Utils.php
+++ plugin-name/includes/Utils.php
@@ -34,6 +34,7 @@ final class Utils {
 			case 'cron':
 				return defined( 'DOING_CRON' ) && DOING_CRON;
 			case 'frontend':
+				return ( ! is_admin() || ( defined( 'DOING_AJAX' ) && DOING_AJAX ) ) && ( ! defined( 'DOING_CRON' ) || ! DOING_CRON ) && ! self::is_rest_api_request();
 				return ( ! is_admin() || ( defined( 'DOING_AJAX' ) && DOING_AJAX ) ) && ( ! defined( 'DOING_CRON' ) || ! DOING_CRON );
 		}
 	}
@@ -81,5 +82,30 @@ final class Utils {
 	 */
 	public static function ajax_url() {
 		return admin_url( 'admin-ajax.php', 'relative' );
+	}
+
+	/**
+	 * Determines if incoming request is towards the REST API.
+	 *
+	 * @return boolean
+	 */
+	protected static function is_rest_api_request() {
+		if ( function_exists( 'WC' ) && method_exists( WC(), 'is_rest_api_request' ) ) {
+			return WC()->is_rest_api_request();
+		}
+
+		if ( empty( $_SERVER['REQUEST_URI'] ) ) {
+			return false;
+		}
+
+		$rest_prefix         = trailingslashit( rest_get_url_prefix() );
+		$is_rest_api_request = ( false !== strpos( $_SERVER['REQUEST_URI'], $rest_prefix ) ); // phpcs:disable WordPress.Security.ValidatedSanitizedInput.MissingUnslash, WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
+
+		/**
+		 * Whether this is a REST API request.
+		 *
+		 * @since 3.6.0
+		 */
+		return apply_filters( 'woocommerce_is_rest_api_request', $is_rest_api_request );
 	}
 }
